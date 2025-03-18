1cab77b8c6c9961c7c6152d96902d18ca1c6ac1d
swarnavs <swarnav@capitalnumbers.com>
2025-03-19 00:31:36 +0530

    Add new home page and modify content

--- app/page.js
+++ app/page.js
@@ -1,3 +1,49 @@
-export default function Home() {
-  return <div>Home page</div>;
+import H1 from "@/components/h1";
+import { getPosts } from "@/lib/posts";
+import Link from "next/link";
+
+export default async function Home() {
+  const { posts } = await getPosts({
+    newest: true,
+    limit: 3,
+  });
+  return (
+    <>
+      <section className="mb-8">
+        <H1>Welcome to my page!</H1>
+        <p>It's a demo project for bloging.</p>
+        <p>
+          Checkout my{" "}
+          <Link href="/about/projects" className="underline">
+            projects
+          </Link>
+          ,{" "}
+          <Link href="/photos" className="underline">
+            photos
+          </Link>{" "}
+          and{" "}
+          <Link href="/blog" className="underline">
+            blog
+          </Link>
+          .
+        </p>
+      </section>
+
+      <section>
+        <h2 className="text-lg mb-8">Latest on the blog</h2>
+        <ul className="font-mono">
+          {posts.map((post) => (
+            <li key={post.slug}>
+              <span className="text-gray-400">
+                {post.frontmatter.date}&nbsp;
+              </span>
+              <Link href={`/blog/${post.slug}`} className="underline">
+                {post.frontmatter.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </>
+  );
 }
